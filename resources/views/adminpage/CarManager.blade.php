36aec89ba089755def390f5808b982e4aa7d65da
Ali Hussain <alihussain2001@gmail.com>
2019-05-03 20:33:57 -0400

    Cleaned up a bit in CarManager

--- resources/views/adminpage/CarManager.blade.php
+++ resources/views/adminpage/CarManager.blade.php
@@ -6,16 +6,35 @@
         background: lightgreen;
     }
 
-    table tr:hover:last-child td:first-child {
+    .table-hover tbody tr td {
+        border-top: 2px solid black;
+        border-right: 2px solid black;
+    }
+
+    .table-hover thead tr th {
+        border-right: 2px solid black;
+    }
+
+    .table-hover thead tr th:last-child {
+        border-right: 0px solid black;
+    }
+
+    .table-hover tbody tr td:last-child {
+        border-top: 2px solid black;
+        border-right: 0px;
+
+    }
+
+    table tr:last-child td:first-child {
     border-bottom-left-radius: 14px;
     }
     
-    table tr:hover:last-child td:last-child {
+    table tr:last-child td:last-child {
     border-bottom-right-radius: 14px;
     }
 
     .table > thead > tr > th, .table > tbody > tr > th, .table > tfoot > tr > th, .table > thead > tr > td, .table > tbody > tr > td, .table > tfoot > tr > td {
-        border-top: 3px solid black;
+        border-top: 2px solid black;
     }
 
     .table > thead > tr > th {
